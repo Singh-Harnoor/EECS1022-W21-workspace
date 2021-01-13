da2a01f9d17e133b7d72ca496575f264884ae3ac
Harnoor <Harnoor249@gmail.com>
2021-01-13 18:33:38 +0530

    Added input from user
    name input

--- Week_01/src/HelloWorld.java
+++ Week_01/src/HelloWorld.java
@@ -1,9 +1,13 @@
-
+import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("My name is Harnoor Singh!");
+		System.out.println("What's your name?");
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+		System.out.println( "Hello "+ name);
 	}
 
 }
