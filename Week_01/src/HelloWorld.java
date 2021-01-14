67e1d3b0b183cda17001dbb5bbfafd40f1df52b3
Harnoor <Harnoor249@gmail.com>
2021-01-14 13:47:40 +0530

    Removed input from the user
    Name input from the user removed

--- Week_01/src/HelloWorld.java
+++ Week_01/src/HelloWorld.java
@@ -4,10 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("My name is Harnoor Singh!");
-		System.out.println("What's your name?");
-		Scanner input = new Scanner(System.in);
-		String name = input.next();
-		System.out.println( "Hello "+ name);
+		
 	}
 
 }
