edb72a1c1540ce5c16a74ccd65e1717544f77de8
Harnoor <Harnoor249@gmail.com>
2021-01-14 23:43:03 +0530

    Create Circle
    Circle class was created to use the area method

---
+++ Week_01/src/Circle.java
@@ -0,0 +1,13 @@
+
+public class Circle {
+	/*
+	 * Utility methods: no need to create objects in order to use them.
+	 */
+	public static double getArea(double radius) {
+		double area = 0.0;
+	    area = 3.14 * radius * radius;
+		return area;
+		  
+	}
+
+}
